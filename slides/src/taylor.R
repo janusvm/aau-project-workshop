58f62384b31ca3bea6c7bef6324b653fe9e903bc
Mads <mads@math.aau.dk>
2019-08-16 11:47:51 +0200

    slides2

---
+++ slides/src/taylor.R
@@ -0,0 +1,15 @@
+pdf("../img/taylor.pdf") 
+f  <- function(x) log(x)
+f1 <- function(x) 1/x
+f2 <- function(x) -1/x^2
+t  <- function(x) f(1) + f1(1) * (x - 1) + f2(1) * (x - 1)^2 / 2
+curve(f, 0.01, 2, lwd = 4, cex.axis = 2, cex.lab = 3, ylab = "")
+curve(t, 0.5,  2, lwd = 4, col = "red", add = TRUE)
+legend(0, 0.8,
+  legend = c("f(x)", "T(x)"),
+  col    = c("black", "red"),
+  lty    = c(1,1),
+  cex    = 1.6,
+  lwd    = 3.5
+)
+dev.off()
